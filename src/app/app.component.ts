a7432d5b9dc792d0b9086aa964aee0d0acb4d376
yevhen.lazieikin <yevhen.lazieikin@devcom.com>
2022-12-23 11:53:13 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from "@angular/forms";
+import {createMask} from "@ngneat/input-mask";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'date-test';
+export class AppComponent implements OnInit {
+  t: Inputmask.Options | undefined;
+  public get getValue(): Date {
+    if (typeof this.textFC.value === "string") {
+      const datearray = (this.textFC.value as unknown as string)?.split("/");
+
+      const newdate = datearray[1] + '/' + datearray[0] + '/' + datearray[2];
+
+      return new Date(newdate);
+
+    } else {
+      return this.textFC.value as Date;
+    }
+
+  }
+
+  textFC = new FormControl(new Date());
+  textInputMask = createMask({
+    alias: 'datetime',
+    inputFormat: 'dd/mm/yyyy',
+    placeholder: 'dd/mm/yyyy'
+    // parser: (value: string) => {
+    //   return new Date(value);
+    // }
+  });
+
+  constructor() {
+
+  }
+
+  public ngOnInit() {
+    this.textFC.valueChanges.subscribe((value) => {
+      console.log('value changed', value);
+    });
+  }
+
+  public onTest(): void {
+    console.log(this.textFC);
+  }
+
+  public onDateChange(value: any): void {
+    this.textFC.setValue(value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputMaskModule } from '@ngneat/input-mask';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MAT_DATE_LOCALE, MatNativeDateModule} from "@angular/material/core";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -10,9 +16,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InputMaskModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
